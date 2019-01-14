30b340ad51400dec9ee4ef83e10985d3e5f8e91b
giangbui <giangbb@viettel.com.vn>
2019-01-14 21:37:20 +0700

    Initial Commit

---
+++ Todoer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoer
+//
+//  Created by Giang Bui Binh on 1/14/19.
+//  Copyright © 2019 giangbb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
